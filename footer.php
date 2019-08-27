cb376ac10353d0933f5dced3ff95759d2855001b
guidemkl <jatupasmkl@gmail.com>
2019-08-27 16:05:59 +0700

    update 8/27/2019

--- footer.php
+++ footer.php
@@ -66,6 +66,36 @@
   <script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap.min.js"></script>
   <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.magnific-popup.min.js"></script>
   <script src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
+  <script>
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+</script>
  
   </body>
 </html>
--- single-camera_product.php
+++ single-camera_product.php
@@ -18,27 +18,61 @@
                 <h3 style="margin-top: 6.0rem"><?php echo get_field('camera_name'); ?></h3>
                    </div>
                        <div class="col-md-6 col-lg-6">
-                         <img src="<?php echo get_field('camera_img'); ?>" style="padding: 4em" alt="Image" class="img-fluid">
+                       <div class="mySlides">
+                        <div class="numbertext">1 / 6</div>
+                        <img src="<?php echo get_field('img_1'); ?>" style="width:100%">
+                      </div>
+
+                      <div class="mySlides">
+                        <div class="numbertext">2 / 6</div>
+                        <img src="<?php echo get_field('img_2'); ?>" style="width:100%">
+                      </div>
+
+                      <div class="mySlides">
+                        <div class="numbertext">3 / 6</div>
+                        <img src="<?php echo get_field('img_4'); ?>" style="width:100%">
+                      </div>
+                        
+                      <div class="mySlides">
+                        <div class="numbertext">4 / 6</div>
+                        <img src="<?php echo get_field('img_5'); ?>" style="width:100%">
+                      </div>
+                       <div class="col-12" style="text-align:center; !important">
+                      <a class="prev" onclick="plusSlides(-1)">❮</a>
+                      <a class="next" onclick="plusSlides(1)">❯</a>
+                      </div> 
+                      <div class="caption-container">
+                        <p id="caption"></p>
+                      </div>
+
+                      <div class="row">
+                        <div class="column col-3 col-md-3 col-lg-3 border-top ">
+                          <img class="demo cursor" src="<?php echo get_field('img_1'); ?>" style="width:100%" onclick="currentSlide(1)" >
+                        </div>
+                        <div class="column col-3 col-md-3 col-lg-3 border-top ">
+                          <img class="demo cursor" src="<?php echo get_field('img_2'); ?>" style="width:100%" onclick="currentSlide(2)" >
+                        </div>
+                        <div class="column col-3 col-md-3 col-lg-3 border-top ">
+                          <img class="demo cursor" src="<?php echo get_field('img_4'); ?>" style="width:100%" onclick="currentSlide(3)" >
+                        </div>
+                        <div class="column col-3 col-md-3 col-lg-3 border-top ">
+                          <img class="demo cursor" src="<?php echo get_field('img_5'); ?>" style="width:100%" onclick="currentSlide(4)" >
+                        </div>
+                      </div>
+                      
                          </div>
                          <div class="col-12 col-md-6 col-lg-6" style="margin-top: 2em">
                             <h3 class="collection d-block"><?php the_title(''); ?></h3>
                             <span class="collection d-block"><?php echo get_field('camera_inform'); ?></span>
                             <strong class="price">$9.50</strong> 
                          </div>
-                       <div class="col-3 col-md-3 col-lg-3 border-top" style="position: relative;">
-                       <img src="<?php echo get_field('img_1'); ?>" alt="Image" class="img-fluid">
-                       </div>
-                       <div class="col-3 col-md-3 col-lg-3 border-top">
-                       <img src="<?php echo get_field('img_2'); ?>" alt="Image" class="img-fluid">
-                       </div>
-                          <div class="col-3 col-md-3 col-lg-3 border-top">
-                       <img src="<?php echo get_field('img_4'); ?>" alt="Image" class="img-fluid">
-                       </div>
-                       <div class="col-3 col-md-3 col-lg-3 border-top">
-                       <img src="<?php echo get_field('img_5'); ?>" alt="Image" class="img-fluid">
-                       </div>
-                <?php endwhile;?>       
+                <?php endwhile;?>  
+               
         </div>
       </div>
     </div>
+    <div class="container">
+  
+</div>
+
 <?php get_footer(); ?>
